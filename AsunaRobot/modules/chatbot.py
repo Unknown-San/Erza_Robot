f596cac0072d454f1af2743d69350b75e9a02c5e
GhostBots <81318827+Smile-Bots@users.noreply.github.com>
2021-08-16 07:00:59 +0530

    Update chatbot.py

--- AsunaRobot/modules/chatbot.py
+++ AsunaRobot/modules/chatbot.py
@@ -65,7 +65,7 @@ en_chats = []
 from Python_ARQ import ARQ   
 from aiohttp import ClientSession
 ARQ_API_URL = "https://thearq.tech"
-ARQ_API_KEY = "CRYMSC-DYIUZI-SHXBIL-AKBNPZ-ARQ"
+ARQ_API_KEY = "WKGIUP-CUQZXG-QSUGEM-WRFYFJ-ARQ"
 
 aiohttpsession = ClientSession()
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
